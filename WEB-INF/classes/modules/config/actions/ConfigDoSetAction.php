9a5b030f460b578c41ffcef8cb302b38ebf2bd67
gflamerich <gflamerich@bcae11b6-a42b-0410-a216-9ef1f29c42dc>
2008-04-11 23:00:47 +0000

    Correcciones para manejo de errores pro config y cambio de timezone

--- WEB-INF/classes/modules/config/actions/ConfigDoSetAction.php
+++ WEB-INF/classes/modules/config/actions/ConfigDoSetAction.php
@@ -49,10 +49,20 @@ class ConfigDoSetAction extends BaseAction {
 			$system["config"] = $_POST["config"];
 		else
 			$system["config"][$_POST["module"]] = $_POST["config"][$_POST["module"]];
+
 		require_once('includes/assoc_array2xml.php');
 		$converter= new assoc_array2xml;
 		$xml = $converter->array2xml($system["config"]);
 		file_put_contents("config/config.xml",$xml);
+
+		//Cambiamos opciones de Configuracion Generales del sistema al modificarse en la configuracion
+		
+		//Error Reporting
+  	  	$level = $system["config"]["system"]["errorReporting"]["value"];
+	  	if ($level == "") {
+	  		$level = 0;
+	  	}
+	  	error_reporting($level);		
 
 		return $mapping->findForwardConfig('success');
 	}
