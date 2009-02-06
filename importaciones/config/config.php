edb66a5398ceaa75a1039c29aecab1d33f5da6e7
gflamerich <gflamerich@bcae11b6-a42b-0410-a216-9ef1f29c42dc>
2009-02-06 13:02:44 +0000

    * Archivos de conexión y classpmap de propel

--- importaciones/config/config.php
+++ importaciones/config/config.php
@@ -2,7 +2,7 @@
 //Archivo de configuracion de directorio de phpmvc
 
 //Directorio donde se encuentra phpmvc
-$appServerRootDir	= "/home/anmagaex/public_html/externals/phpMVC";
+$appServerRootDir	= "/usr/local/lib/php/phpMVCtest";
 //Directorio donde se encuentra la aplicacion (sin barra al final)
 $moduleRootDir = "/home/anmagaex/public_html/WEB-INF";
 //Directorio donde se encuentra PEAR
@@ -16,5 +16,3 @@ $mluse = "esp";
 
 //Cantidad de licencias de usuarios.
 $licenses = "";
-
-?>
