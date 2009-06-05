4399d25cc030e0b9da7f27a91172abe0ceee9cf7
martinrm <martinrm@bcae11b6-a42b-0410-a216-9ef1f29c42dc>
2009-06-05 18:44:41 +0000

    modificaciones a validacion client-side y agregado de validacion via ajax

--- importaciones/scripts/validation.js
+++ importaciones/scripts/validation.js
@@ -5,23 +5,27 @@ function validationValidateFormClienSide(form) {
 	
 	var valid = false;
 	
+	var emptyArray = document.getElementsByClassName('emptyValidation',form);
 	var textArray = document.getElementsByClassName('textValidation',form);
 	var mailArray = document.getElementsByClassName('mailValidation',form);	
 	var numericArray = document.getElementsByClassName('numericValidation',form);
 	var dateArray = document.getElementsByClassName('dateValidation',form);
 	
+	var emptyResult = validationValidateElements(emptyArray, validationEmptyValidator);
 	var textResult = validationValidateElements(textArray, validationTextValidator);
 	var mailResult = validationValidateElements(mailArray, validationMailValidator);
 	var numericResult = validationValidateElements(numericArray, validationNumericValidator);
 	var dateResult = validationValidateElements(dateArray, validationDateValidator);	
 
-	valid = ((textResult.length == 0) && (mailResult.length == 0) && (numericResult.length == 0) && (dateResult.length == 0))
+	valid = ((emptyResult.length == 0) && (textResult.length == 0) && (mailResult.length == 0) && (numericResult.length == 0) && (dateResult.length == 0))
 
 	if (valid == false) {
 		validationSetInvalidFields(textResult);
 		validationSetInvalidFields(mailResult);
 		validationSetInvalidFields(numericResult);
 		validationSetInvalidFields(dateResult);
+		
+		alert('Algunos Campos Han Resultado Invalidos, No se Ha enviado el Formulario');
 	}
 	else {
 		form.submit();
@@ -54,19 +58,73 @@ function validationValidateElements(elements, validationFunction) {
 	
 }
 
+function validationEmptyValidator(element) {
+
+	if (element.value == '') {
+		return false;
+	}
+
+	return true;
+}
 
 function validationTextValidator(element) {
-	return true;
+	
+	if (validationEmptyValidator(element) == false)
+		return false;
+	
+	return validateField(element, 'txt');
 }
 
 function validationMailValidator(element) {
-	return true;
+
+	if (validationEmptyValidator(element) == false)
+		return false;
+
+	return validateField(element, 'email');
+
 }
 
 function validationNumericValidator(element) {
-	return true;
+
+	if (validationEmptyValidator(element) == false)
+		return false;
+
+	return validateField(element, 'num');
+
 }
 
 function validationDateValidator(element) {
-	return false;
+
+	if (validationEmptyValidator(element) == false)
+		return false;
+	
+	return validateField(element, 'date');
+	
+}
+
+function validationValidateFieldThruAjax(element,doAction) {
+
+	var url = 'Main.php?do=' + doAction;
+	var fields = element.name + '=' + element.value; 
+	var myAjax = new Ajax.Request(
+				url,
+				{
+					method: 'post',
+					postBody: fields,
+					onSuccess: function(transport) {
+						var response = transport.responseText.evalJSON();
+
+						if (response.value == 1) {
+							//es invalido
+						}
+						
+						var elementName = response.name + '_box';
+						var element = $(response.name + '_box');
+						if (element != null)
+							element.innerHTML = response.message;
+												
+					}
+					
+				});
+
 }
