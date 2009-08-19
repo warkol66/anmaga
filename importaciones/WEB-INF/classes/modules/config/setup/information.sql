1335d92b5c94ea4b3197da27921f8de0231227c1
gflamerich <gflamerich@bcae11b6-a42b-0410-a216-9ef1f29c42dc>
2009-08-19 17:39:20 +0000

    * Elimino accions obsoletos * Agrego archivos de instalacion y template ocn js

--- importaciones/WEB-INF/classes/modules/config/setup/information.sql
+++ importaciones/WEB-INF/classes/modules/config/setup/information.sql
@@ -1,6 +1,3 @@
-DELETE FROM `modules_label` WHERE `name` = 'config';
-INSERT INTO `modules_label` ( `name` , `label` , `description` , `language` ) VALUES ('config', 'Configuración', 'Módulo para manejo de la configuración del sistema', 'esp');
-INSERT INTO `modules_label` ( `name` , `label` , `description` , `language` ) VALUES ('config', 'Config', 'Config management', 'eng');
-DELETE FROM `modules_dependency` WHERE `moduleName` = 'config';
 DELETE FROM `modules_module` WHERE `name` = 'config';
-INSERT INTO `modules_module` ( `name` , `active` , `alwaysActive` ) VALUES ('config', '1', '1');
+INSERT INTO `modules_module` ( `name` , `active` , `alwaysActive`, `hasCategories` ) VALUES ('config', '1', '1','');
+DELETE FROM `modules_dependency` WHERE `moduleName` = 'config';
