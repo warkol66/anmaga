14c3c5a8a3a83fbf89fefd5af6aa79615112b84d
gflamerich <gflamerich@bcae11b6-a42b-0410-a216-9ef1f29c42dc>
2010-05-03 11:31:05 +0000

    * Agrego replicas del SAF

---
+++ replicas/evocare/config/config.php
@@ -0,0 +1,22 @@
+<?php
+//Archivo de configuracion de directorio de phpmvc
+
+//Directorio donde se encuentra phpmvc
+$appServerRootDir	= "/home/evocare/public_html/externals/phpMVC";
+
+//Directorio donde se encuentra la aplicacion (sin barra al final)
+$moduleRootDir = "/home/evocare/public_html";
+
+//Directorio donde se encuentra PEAR
+$pearDir = "";
+
+//Sistema operativo [UNIX|WINDOWS|MAC]
+$osType = "UNIX";
+
+//Codigo del idioma actual.
+$mluse = "";
+
+//Cantidad de licencias de usuarios.
+$licenses = "";
+
+?>
