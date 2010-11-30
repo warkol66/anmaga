bd3b2499fc1c5b4c7e715332575a360bbc35a5a8
gflamerich <gflamerich@bcae11b6-a42b-0410-a216-9ef1f29c42dc>
2010-11-30 10:54:44 +0000

    * Reemplazo el ModulePaths pro el dinamico * Agrego el archivo ChangeLog.txt por ahora vacío * Copio los schemas a la nueva ubicación, abajo del WEB-INF/propel, luego borrar la carpeta actual

--- WEB-INF/ModulePaths.php
+++ WEB-INF/ModulePaths.php
@@ -11,116 +11,36 @@ class ModulePaths {
 
 	/**
 	* Return an array of global paths
-	* 
+	*
 	* @public
-	* @returns array	
+	* @returns array
 	*/
 	function getModulePaths() {
 
-		// Setup the main module include() directories here
-		// Note: could be placed in an xml config file later !!
+		//Setup the main module include() directories here
+		//Note: could be placed in an xml config file later !!
 		$appDirs		= array();
 		$appDirs[]	= ''; // starting with the sub-application home directory
 		$appDirs[]	= 'WEB-INF';
 		$appDirs[]	= 'WEB-INF/classes';
 		$appDirs[]	= 'WEB-INF/configRules';
 		$appDirs[]	= 'WEB-INF/tpl';
-		$appDirs[]	= 'WEB-INF/scripts';
 		$appDirs[]	= 'WEB-INF/classes/includes';
 		$appDirs[]	= 'WEB-INF/classes/modules';
+
+		//Path a modulos
+		$path = "WEB-INF/classes/modules";
+		$modules = scandir($path);
 		
-		//Common
-		$appDirs[]      = 'WEB-INF/classes/modules/common';
-		$appDirs[]      = 'WEB-INF/classes/modules/common/actions';
-		$appDirs[]      = 'WEB-INF/classes/modules/common/classes';				
-
-		// Users module
-		$appDirs[]	= 'WEB-INF/classes/modules/users';
-		$appDirs[]	= 'WEB-INF/classes/modules/users/actions';
-		$appDirs[]	= 'WEB-INF/classes/modules/users/classes';
-
-		// Htmls module
-		$appDirs[]	= 'WEB-INF/classes/modules/htmls';
-		$appDirs[]	= 'WEB-INF/classes/modules/htmls/actions';
-		$appDirs[]	= 'WEB-INF/classes/modules/htmls/classes';
-		
-		// Config module
-		$appDirs[]	= 'WEB-INF/classes/modules/config';
-		$appDirs[]	= 'WEB-INF/classes/modules/config/actions';
-		$appDirs[]	= 'WEB-INF/classes/modules/config/classes';
-
-		// Security module
-		$appDirs[]	= 'WEB-INF/classes/modules/security';
-		$appDirs[]	= 'WEB-INF/classes/modules/security/actions';
-		$appDirs[]	= 'WEB-INF/classes/modules/security/classes';
-
-		// Affiliates module
-		$appDirs[]	= 'WEB-INF/classes/modules/affiliates';
-		$appDirs[]	= 'WEB-INF/classes/modules/affiliates/actions';
-		$appDirs[]	= 'WEB-INF/classes/modules/affiliates/classes';
-
-		// Users By Affiliate module
-		$appDirs[]	= 'WEB-INF/classes/modules/usersByAffiliate';
-		$appDirs[]	= 'WEB-INF/classes/modules/usersByAffiliate/actions';
-		$appDirs[]	= 'WEB-INF/classes/modules/usersByAffiliate/classes';
-		
-		// Categories module
-		$appDirs[]	= 'WEB-INF/classes/modules/categories';
-		$appDirs[]	= 'WEB-INF/classes/modules/categories/actions';
-		$appDirs[]	= 'WEB-INF/classes/modules/categories/classes';
-
-		// Catalog module
-		$appDirs[]	= 'WEB-INF/classes/modules/catalog';
-		$appDirs[]	= 'WEB-INF/classes/modules/catalog/actions';
-		$appDirs[]	= 'WEB-INF/classes/modules/catalog/classes';
-
-				
-		// Actionlogs module
-		$appDirs[]	= 'WEB-INF/classes/modules/actionlogs';
-		$appDirs[]	= 'WEB-INF/classes/modules/actionlogs/actions';
-		$appDirs[]	= 'WEB-INF/classes/modules/actionlogs/classes';
-				
-		// Modules module
-		$appDirs[]	= 'WEB-INF/classes/modules/modules';
-		$appDirs[]	= 'WEB-INF/classes/modules/modules/actions';
-		$appDirs[]	= 'WEB-INF/classes/modules/modules/classes';
-		
-		// Orders module
-		$appDirs[]	= 'WEB-INF/classes/modules/orders';
-		$appDirs[]	= 'WEB-INF/classes/modules/orders/actions';
-		$appDirs[]	= 'WEB-INF/classes/modules/orders/classes';
-		
-		//importaciones
-		$appDirs[]      = 'WEB-INF/classes/modules/import';
-		$appDirs[]      = 'WEB-INF/classes/modules/import/actions';
-		$appDirs[]      = 'WEB-INF/classes/modules/import/classes';
-		
-		//install
-		$appDirs[]      = 'WEB-INF/classes/modules/install';
-		$appDirs[]      = 'WEB-INF/classes/modules/install/actions';
-		$appDirs[]      = 'WEB-INF/classes/modules/install/classes';
-
-		// Logs module
-		$appDirs[]	= 'WEB-INF/classes/modules/actionLogs';
-		$appDirs[]	= 'WEB-INF/classes/modules/actionLogs/actions';
-		$appDirs[]	= 'WEB-INF/classes/modules/actionLogs/classes';			
-
-		//Backup
-		$appDirs[]      = 'WEB-INF/classes/modules/backup';
-		$appDirs[]      = 'WEB-INF/classes/modules/backup/actions';
-		$appDirs[]      = 'WEB-INF/classes/modules/backup/classes';
-		
-		//Multilang
-		$appDirs[]      = 'WEB-INF/classes/modules/multilang';
-		$appDirs[]      = 'WEB-INF/classes/modules/multilang/actions';
-		$appDirs[]      = 'WEB-INF/classes/modules/multilang/classes';
-
-		//Validation
-		$appDirs[]      = 'WEB-INF/classes/modules/validation';
-		$appDirs[]      = 'WEB-INF/classes/modules/validation/actions';
-		$appDirs[]      = 'WEB-INF/classes/modules/validation/classes';				
+		foreach ($modules as $module)
+			if (substr("$module", -1) != "." && $module != ".svn" && is_dir($path.'/'.$module)){
+				$appDirs[]	= "WEB-INF/classes/modules/$module";
+				$appDirs[]	= "WEB-INF/classes/modules/$module/actions";
+				$appDirs[]	= "WEB-INF/classes/modules/$module/classes";
+			}
 
 		return $appDirs;
+
 
 	}
 
