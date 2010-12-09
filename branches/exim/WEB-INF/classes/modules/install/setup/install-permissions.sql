4ca2184b7e5495385c9beebb3cacbfdca5a8a9d4
gflamerich <gflamerich@bcae11b6-a42b-0410-a216-9ef1f29c42dc>
2010-12-09 15:24:56 +0000

    * Elimino el install, ahora va dentro de modules

--- branches/exim/WEB-INF/classes/modules/install/setup/install-permissions.sql
+++
@@ -1,9 +0,0 @@
-DELETE FROM `security_module` WHERE `module` = 'install';
-INSERT INTO `security_module` ( `module` , `noCheckLogin` , `access` , `accessAffiliateUser` , `accessRegistrationUser` ) VALUES ('install', '1', '0', '0','0');
-DELETE FROM `security_action` WHERE `module` = 'install';
-INSERT INTO `security_action` (`action`,`module`,`section`,`access`,`accessAffiliateUser`, `active` , `pair` , `noCheckLogin`, `accessRegistrationUser` ) VALUES ('installModuleInformationSetup','install','','9','0','1','','','0' );
-INSERT INTO `security_action` (`action`,`module`,`section`,`access`,`accessAffiliateUser`, `active` , `pair` , `noCheckLogin`, `accessRegistrationUser` ) VALUES ('installFileCheck','install','','0','0','1','installDoFileCheck','','0' );
-INSERT INTO `security_action` (`action`,`module`,`section`,`access`,`accessAffiliateUser`, `active` , `pair` , `noCheckLogin`, `accessRegistrationUser` ) VALUES ('installSetupActionsLabel','install','','0','0','1','installDoSetupActionsLabel','','0' );
-INSERT INTO `security_action` (`action`,`module`,`section`,`access`,`accessAffiliateUser`, `active` , `pair` , `noCheckLogin`, `accessRegistrationUser` ) VALUES ('installSetupMessages','install','','0','0','1','installDoSetupMessages','','0' );
-INSERT INTO `security_action` (`action`,`module`,`section`,`access`,`accessAffiliateUser`, `active` , `pair` , `noCheckLogin`, `accessRegistrationUser` ) VALUES ('installSetupModuleInformation','install','','0','0','1','installDoSetupModuleInformation','','0' );
-INSERT INTO `security_action` (`action`,`module`,`section`,`access`,`accessAffiliateUser`, `active` , `pair` , `noCheckLogin`, `accessRegistrationUser` ) VALUES ('installSetupPermissions','install','','0','0','1','installDoSetupPermissions','','0' );
--- branches/exim/WEB-INF/classes/modules/install/setup/messages_esp.sql
+++
@@ -1,3 +0,0 @@
-DELETE FROM `actionLogs_label` WHERE `action` LIKE 'Install%' AND `language` = 'esp';
-INSERT INTO `actionLogs_label` (`action`, `label`, `language`, `forward`) VALUES ( 'InstallSetupMessages', 'Módulo instalado correctamente','esp','success');
-INSERT INTO `actionLogs_label` (`action`, `label`, `language`, `forward`) VALUES ( 'InstallSetupMessages', 'Error al instalar el módulo','esp','failure');
