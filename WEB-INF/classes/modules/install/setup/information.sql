77dfdf9fc2c7727d44436337c32a7a35ae865455
gflamerich <gflamerich@bcae11b6-a42b-0410-a216-9ef1f29c42dc>
2010-12-09 15:24:56 +0000

    * Elimino el install, ahora va dentro de modules

--- WEB-INF/classes/modules/install/setup/information.sql
+++
@@ -1,3 +0,0 @@
-DELETE FROM `modules_dependency` WHERE `moduleName` = 'install';
-DELETE FROM `modules_module` WHERE `name` = 'install';
-INSERT INTO `modules_module` ( `name` , `active` , `alwaysActive`, `hasCategories` ) VALUES ('install', '1', '','');
--- WEB-INF/classes/modules/install/setup/modulesLabel_esp.sql
+++
@@ -1,2 +0,0 @@
-DELETE FROM `modules_label` WHERE `name` = 'install' AND `language` = 'esp';
-INSERT INTO `modules_label` ( `name` , `label` , `description` , `language` ) VALUES ('install', 'Instalador', 'Módulo para instalación y generación de información de instalación', 'esp');
