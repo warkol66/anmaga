40ac8423fe6ddef4d72813611f609c4c8cad5cb6
gflamerich <gflamerich@bcae11b6-a42b-0410-a216-9ef1f29c42dc>
2010-12-24 19:49:35 +0000

    * Agrego los actions, templates y clases del common vigente

---
+++ WEB-INF/classes/modules/common/actions/CommonSendAlertsAction.php
@@ -0,0 +1,43 @@
+<?php
+require_once("EmailManagement.php");
+
+class CommonSendAlertsAction extends BaseAction {
+
+	function CommonSendAlertsAction() {
+		;
+	}
+
+	function execute($mapping, $form, &$request, &$response) {
+
+		BaseAction::execute($mapping, $form, $request, $response);
+
+		$this->template->template = "TemplatePlain.tpl";
+
+		//////////
+		// Access the Smarty PlugIn instance
+		// Note the reference "=&"
+		$plugInKey = 'SMARTY_PLUGIN';
+		$smarty =& $this->actionServer->getPlugIn($plugInKey);
+		if($smarty == NULL) {
+			echo 'No PlugIn found matching key: '.$plugInKey."<br>\n";
+		}
+				
+		$alertsSubscriptions = AlertSubscriptionPeer::getAll();
+		$totalRecipients = array();
+		
+		foreach($alertsSubscriptions as $alertSubscription) {
+			$entitiesFiltered = $alertSubscription->getEntitiesFiltered();
+			if (!empty($entitiesFiltered) && count($entitiesFiltered) > 0) {
+				$recipients = $alertSubscription->getRecipients();
+				$subject = Common::getTranslation('Alert','users');
+				$smarty->assign('alertSubscription', $alertSubscription);
+				$body = $smarty->fetch('CommonAlertMail.tpl');
+				$partialRecipients = AlertSubscriptionPeer::sendAlert($alertSubscription, $body, $recipients, $subject);
+				$totalRecipients = array_merge($totalRecipients, $partialRecipients);
+			}	
+		}
+		$smarty->assign('timestamp', new DateTime());
+		$smarty->assign('recipientsCount', count($totalRecipients));
+		return $mapping->findForwardConfig('success');
+	}
+}
---
+++ WEB-INF/classes/modules/common/classes/ActionLog.php
@@ -0,0 +1,80 @@
+<?php
+
+
+/**
+ * Skeleton subclass for representing a row from the 'actionLogs_log' table.
+ *
+ * logs del sistema
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ * @package    propel.generator.common.classes
+ */
+class ActionLog extends BaseActionLog {
+
+	/**
+	*
+	* Obtiene la etiqueta de ese modulo
+	*
+	* @return string label la etiqueta
+	*/
+	
+	function getLabel(){
+		
+		try{
+		include_once 'ActionLogLabelPeer.php';
+		global $system;
+		$language = Common::getCurrentLanguageCode();
+		if(empty($language))
+			$language='eng';
+		$logLabelInfo=ActionLogLabelPeer::getByInfo($this->GetAction(),$this->GetForward(),$language);
+		return $logLabelInfo;
+		}catch (PropelException $e) {}
+	}
+
+	/**
+	* getActionLabel
+	* Obtiene la etiqueta del action
+	*
+	* @return string label la etiqueta
+	*/
+	
+	function getActionLabel(){
+		
+		try{
+			include_once 'SecurityActionLabelPeer.php';
+	
+			$language = Common::getCurrentLanguageCode();
+			if(empty($language))
+				$language='eng';
+			$actionLabelInfo = SecurityActionLabelPeer::getByActionAndLanguage($this->getAction(),$language);
+			return $actionLabelInfo;
+		}catch (PropelException $e) {}
+	}
+
+	public function getSecurityAction() {
+		
+		$result = parent::getSecurityAction();
+		
+		//si es un action con Do, buscamos la informacion sin el do
+		//ya que en ese caso se da de alta como pair
+		if (empty($result) && (preg_match("/(.*)([a-z]Do[A-Z])(.*)/",$this->getAction(),$parts))) {
+			
+			$actionWithoutDo = $parts[1].$parts[2][0].$parts[2][3].$parts[3];
+			$result = SecurityActionPeer::get($actionWithoutDo);		
+		
+		}
+		
+		return $result;
+	
+	}
+
+	function getAffiliateUser(){
+		$criteria = new Criteria();
+		require_once('AffiliateUserPeer.php');
+		return AffiliateUserPeer::get($this->getAffiliateId());
+	}
+
+} // ActionLog
