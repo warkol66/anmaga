ffc48689bfa1ea46ec0907f45629747c11f8af45
gflamerich <gflamerich@bcae11b6-a42b-0410-a216-9ef1f29c42dc>
2011-02-22 18:50:40 +0000

    * Información de setup para backup y catalog * Fix en BackupList.tpl

--- branches/anmaga_update/WEB-INF/classes/modules/backup/setup/actionLabel_esp.sql
+++ branches/anmaga_update/WEB-INF/classes/modules/backup/setup/actionLabel_esp.sql
@@ -1,11 +1,8 @@
 DELETE FROM `security_actionLabel` WHERE `action` LIKE 'Backup%' AND `language` = 'esp';
 OPTIMIZE TABLE `security_actionLabel`;
-INSERT INTO `security_actionLabel` ( `action` , `label` , `description` ,`language` ) VALUES ('BackupDelete', 'Eliminar respaldo', 'Eliminar respaldo', 'esp');
-INSERT INTO `security_actionLabel` ( `action` , `label` , `description` ,`language` ) VALUES ('BackupRestoreFromFile', 'Restaurar respaldo desde archivo', 'Restaurar respaldo desde archivo', 'esp');
-INSERT INTO `security_actionLabel` ( `action` , `label` , `description` ,`language` ) VALUES ('BackupRestore', 'Restaurar respaldo', 'Restaurar respaldo', 'esp');
-INSERT INTO `security_actionLabel` ( `action` , `label` , `description` ,`language` ) VALUES ('BackupCreateToFile', 'Crear respaldo en archvi para descargar', 'Crear respaldo en archvi para descargar', 'esp');
-INSERT INTO `security_actionLabel` ( `action` , `label` , `description` ,`language` ) VALUES ('BackupDownload', 'Descargar archivo de respaldo', 'Descargar archivo de respaldo', 'esp');
-INSERT INTO `security_actionLabel` ( `action` , `label` , `description` ,`language` ) VALUES ('BackupSendByEmail', 'Enviar respaldo por mail', 'Enviar respaldo por mail', 'esp');
-INSERT INTO `security_actionLabel` ( `action` , `label` , `description` ,`language` ) VALUES ('BackupCreate', 'Crear respaldo en servidor', 'Crear respaldo en servidor', 'esp');
-INSERT INTO `security_actionLabel` ( `action` , `label` , `description` ,`language` ) VALUES ('BackupSendByEmailCron', 'Crear y enviar respaldo por mail mediante cron', 'Crear y enviar respaldo por mail mediante cron', 'esp');
-INSERT INTO `security_actionLabel` ( `action` , `label` , `description` ,`language` ) VALUES ('BackupList', 'Listado de respaldos disponibles', 'Listado de respaldos disponibles', 'esp');
+INSERT INTO `security_actionLabel` ( `action` , `label` , `description` ,`language` ) VALUES ('BackupDelete', 'Eliminar respaldo', 'Elimina respaldo guardado en el servidor', 'esp');
+INSERT INTO `security_actionLabel` ( `action` , `label` , `description` ,`language` ) VALUES ('BackupRestore', 'Restaurar respaldo', 'Restaura la información guardada en un archivo de respaldo', 'esp');
+INSERT INTO `security_actionLabel` ( `action` , `label` , `description` ,`language` ) VALUES ('BackupDownload', 'Descargar archivo de respaldo', 'Descarga archivo de respaldo a equipo local', 'esp');
+INSERT INTO `security_actionLabel` ( `action` , `label` , `description` ,`language` ) VALUES ('BackupSendByEmail', 'Enviar respaldo por mail', 'Enviar archivo de respaldo por correo electrónico', 'esp');
+INSERT INTO `security_actionLabel` ( `action` , `label` , `description` ,`language` ) VALUES ('BackupCreate', 'Crear respaldo en servidor', 'Crea archivo de respaldo en servidor', 'esp');
+INSERT INTO `security_actionLabel` ( `action` , `label` , `description` ,`language` ) VALUES ('BackupList', 'Listado de respaldos disponibles', 'Muestra el listado de respaldos disponibles en el servidor', 'esp');
