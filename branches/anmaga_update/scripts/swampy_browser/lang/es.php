3a41e0d892604dd2cd2f84171b61525deb44db37
gflamerich <gflamerich@bcae11b6-a42b-0410-a216-9ef1f29c42dc>
2011-02-24 17:20:52 +0000

    * Tiny y Swampy para edición de textos html

---
+++ branches/anmaga_update/scripts/swampy_browser/lang/es.php
@@ -0,0 +1,111 @@
+﻿<?php
+/* Common */
+$DLG['title'] = 	  "Administrador de Archivos e Im&aacute;genes";
+
+/* Common */
+$DLG['select_dir'] = 	  "Seleccine primero el directorio!";
+$DLG['dir_not_found'] = "Directorio no encontrado!";
+$DLG['not_dir'] = 	    "No es un directorio!";
+$DLG['invalid_dir'] = 	"No es un directorio v&aacute;lido!";
+$DLG['no_permission'] = "No hay permisos de escritura!";
+
+/* Add Dir */
+$DLG['invalid_dirname'] = "Nombre de directorio inv&aacute;lido!";
+$DLG['dir_exists'] =		  "El directorio ya existe!";
+$DLG['dir_add_success'] =	"Directorio creado";
+$DLG['dir_add_failure'] =	"Error creando directorio!";
+
+/* File listing */
+$DLG['enter'] =		 "Entrar";
+$DLG['delete'] =	 "Eliminar";
+$DLG['insert'] =	 "Insertar";
+$DLG['download'] = "Descargar";
+$DLG['preview'] =	 "Vista previa";
+$DLG['rename'] =	 "Renombrar";
+$DLG['dir'] = 		 "Directorio";
+$DLG['up'] = 		   "Subir";
+
+/* Uploads */
+$DLG['select_file'] =		"Seleccione primero un archivo!";
+$DLG['invalid_filename'] =	"Nombre de archivo inv&aacute;lido!";
+$DLG['file_exists'] =		"El nombre de archivo ya existe!";
+$DLG['invalid_img'] =		"Tipo de imagen inv&aacute;lido!";
+$DLG['insert_uploaded'] = 	"Haga click aqu&iacute; para insertar un archivo";
+$DLG['upload_success'] = 	"Subido exitosamente";
+$DLG['upload_failure'] =	"Error subiendo archivo!";
+
+$DLG['select_to_upload'] = 	"Seleccione archivo a subir";
+$DLG['select_img_format'] = 	"Seleccionar formato(s) de imagen";
+$DLG['file_to_upload'] = 	"Archivo a subir";
+$DLG['as_file_name'] = 		"Con el nombre";
+$DLG['uploading'] = 		"Subiendo";
+$DLG['upload'] = 		"Subir";
+
+$DLG['format'] = 	"Formato";
+$DLG['width'] = 	"Ancho";
+$DLG['height'] = 	"Alto";
+$DLG['scale_type'] = 	"Tipo de escala";
+$DLG['bg'] = 		"Fondo";
+$DLG['custom'] = 	"Personalizado";
+
+/* Delete */
+$DLG['file_not_found'] = 	"Archivo no encontrado!";
+$DLG['dir_not_found'] = 	"Directorio no encontrado!";
+$DLG['not_file'] = 		"No se pudo eliminar archivo, no es un archivo!";
+$DLG['not_dir'] = 		"No se pudo eliminar directorio, no es un directorio!";
+$DLG['file_delete_success'] = 	"Archivo eliminado";
+$DLG['file_delete_failure'] = 	"No se pudo eliminar archivo!";
+$DLG['dir_delete_success'] = 	"Directorio eliminado exitosamente";
+$DLG['dir_delete_failure'] = 	"No se pudo eliminar directorio!";
+$DLG['dir_not_empty'] = 	"No se pudo eliminar directorio, el directorio no esta vac&iacute;o";
+
+/* Index */
+$DLG['name'] =		"Nombre";
+$DLG['extension'] =	"Tipo/Extensi&oacute;n";
+$DLG['size'] =		"Tama&ntilde;o";
+$DLG['dimentions'] =	"Dimensiones";
+$DLG['upload_image'] =	"Subir imagen";
+$DLG['upload_file'] =	"Subir archivo";
+$DLG['add_dir'] =	"Agregar directorio";
+$DLG['path'] =		"Ruta";
+$DLG['view_mode'] =	"Modo de vista";
+
+/* format listing  */
+$DLG['select_format'] = 	"Seleccione el tipo de imagen para insertar o descargar";
+
+/* Preview */
+$DLG['not_supported_format'] = 	"Formato no disponible para vista previa";
+
+/* Rename */
+$DLG['rename_success'] = 	"renombrado a";
+$DLG['rename_failure'] =	"Error renombrando!";
+
+/* Misc */
+$JAVA_DLG = 'var Lang = {
+	initiating :		"Iniciando...",
+	loading :		"Cargando...",
+	done :			"Terminado",
+	uploading :		"Subiendo...",
+	loading_failure :	"No se pudo subir el contenido",
+	file_delete_confirm :	"Esta seguro que desea eliminar",
+	folder_name_prompt :	"Ingrese el nombre para el directorio que desea agregar",
+	rename_prompt :		"Renombrar, ingrese el nuevo nombre",
+	not_supported : 	"Operaci&oacute;n no disponible a&uacute;n",
+	opt_enter :		"Enter",
+	opt_delete :		"Eliminar",
+	opt_insert :		"Insertar",
+	opt_download :		"Descargar",
+	opt_preview :		"Vista previa",
+	opt_rename :		"Renombrar",
+	dir :			"Directorio",
+	up :			"Subir"
+};';
+
+$SCALE_TYPES = array
+(
+	'orginal' =>	"Tama&ntilde;o original",
+	'max' =>	"Tama&ntilde;o m&aacute;ximo de ajuste",
+	'addbg' =>	"Agregar fondo",
+	'crop' =>	"Cortar hacie el centro",
+	'stretch' =>	"Estirar imagen",
+);
