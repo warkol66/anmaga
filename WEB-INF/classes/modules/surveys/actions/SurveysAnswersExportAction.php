20f14f4a44856e09d43801302c358754d4925f12
gflamerich <gflamerich@bcae11b6-a42b-0410-a216-9ef1f29c42dc>
2011-03-15 23:14:36 +0000

    * Bug en el total de encuestas y la exportación

--- WEB-INF/classes/modules/surveys/actions/SurveysAnswersExportAction.php
+++ WEB-INF/classes/modules/surveys/actions/SurveysAnswersExportAction.php
@@ -35,13 +35,12 @@ class SurveysAnswersExportAction extends BaseAction {
 		
 		foreach ($surveyQuestions as $surveyQuestion) {
 			$answerOptions = $surveyQuestion->getSurveyAnswerOptions();
-			$content .= "'" . $surveyQuestion . "',''\r\n";
+			$content .= "'" . $surveyQuestion . "'\r\n";
 			foreach ($answerOptions as $answerOption) {
 				$count = $answerOption->getAnswerCount();
 				$answer = $answerOption->getAnswer();
 				$content .= "'".$answer."','".$count."'\r\n";
 			}
-			$content .= "'',''\r\n";
 		}
 
 		$filename = str_replace(' ','_',strtolower($surveyQuestion->getQuestion()));
