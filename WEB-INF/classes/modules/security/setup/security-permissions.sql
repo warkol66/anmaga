1891bb02c2b0e7079fadaab26b32b06f1c374932
gflamerich <gflamerich@bcae11b6-a42b-0410-a216-9ef1f29c42dc>
2011-06-22 18:57:56 +0000

    * Fix nombre del action

--- WEB-INF/classes/modules/security/setup/security-permissions.sql
+++ WEB-INF/classes/modules/security/setup/security-permissions.sql
@@ -3,4 +3,4 @@ OPTIMIZE TABLE `security_module`;
 INSERT INTO `security_module` ( `module` , `noCheckLogin` , `access` , `accessAffiliateUser` , `accessRegistrationUser` ) VALUES ('security', '', '3', '0','0');
 DELETE FROM `security_action` WHERE `module` = 'security';
 OPTIMIZE TABLE `security_action`;
-INSERT INTO `security_action` (`action`,`module`,`section`,`access`,`accessAffiliateUser`, `active` , `pair` , `noCheckLogin`, `accessRegistrationUser` ) VALUES ('noPermission','security','','0','0','1','','1','0' );
+INSERT INTO `security_action` (`action`,`module`,`section`,`access`,`accessAffiliateUser`, `active` , `pair` , `noCheckLogin`, `accessRegistrationUser` ) VALUES ('securityNoPermission','security','','0','0','1','','1','0' );
