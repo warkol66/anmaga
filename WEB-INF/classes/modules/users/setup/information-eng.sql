7cc3c08c6cab0f3b15170cad3eb27f6659aa0b5d
gflamerich <gflamerich@bcae11b6-a42b-0410-a216-9ef1f29c42dc>
2011-06-22 16:34:50 +0000

    * Actualización de la información de instalación del módulo users

--- WEB-INF/classes/modules/users/setup/information-eng.sql
+++
@@ -1,2 +0,0 @@
-DELETE FROM `modules_label` WHERE `name` = 'users' AND `language` = 'eng';
-INSERT INTO `modules_label` ( `name` , `label` , `description` , `language` ) VALUES ('users', 'Users', 'Users Management', 'eng');
--- WEB-INF/classes/modules/users/setup/information-esp.sql
+++
@@ -1,2 +0,0 @@
-DELETE FROM `modules_label` WHERE `name` = 'users' AND `language` = 'esp';
-INSERT INTO `modules_label` ( `name` , `label` , `description` , `language` ) VALUES ('users', 'Usuarios', 'Módulo de Administración de Usuarios', 'esp');
