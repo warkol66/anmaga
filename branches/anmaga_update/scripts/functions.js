5ac6b99e0805c38c78e0c117bc1fe042feba6918
gflamerich <gflamerich@bcae11b6-a42b-0410-a216-9ef1f29c42dc>
2011-10-14 12:21:19 +0000

    * Javascripts e sincludes actualizados

--- branches/anmaga_update/scripts/functions.js
+++ branches/anmaga_update/scripts/functions.js
@@ -1,6 +1,85 @@
+function setClass(elementId,className) {
+	var element = document.getElementById(elementId);
+	element.className = className;
+} // End of setClass
+
+function setClassMultiple(elements,className){
+	var i=0;
+	for(i=0; i<elements.length; i++){
+		setClass(elements[i],className);
+	}
+} // End of setClassMultiple
+
+function setElementClass(elementId,className) {
+	var element = document.getElementById(elementId);
+	element.className = className;
+} // End of setElementClass
+
+function switchDisplay(element,display){
+	var element_ref = "";
+	var undefinedDisplayValue = 0;
+	var switchDisplayValue = "";
+	element_ref = document.getElementById(element);
+	if (display == undefined){
+		undefinedDisplayValue = 1;
+	}
+	if (element_ref.style.display != 'none' && element_ref.style.display != "" && display == undefined){
+		display = 'none';
+	}
+	else{
+		if (undefinedDisplayValue == 1 && switchDisplayValue != display) {
+			display = switchDisplayValue;
+		}
+	}
+	element_ref.style.display=display;
+} // End of switchDisplay
+
+function switchDisplayMultiple(elements,display){
+	if (display == undefined){
+		switchDisplayValue = 'none';
+	}
+	var i=0;
+	for(i=0; i<elements.length; i++){
+		switchDisplay(elements[i],display);
+	}
+} // End of switchDisplayMultiple
+
 function logout(){
 	return window.confirm("Esta seguro que quiere salir del sistema?")
 } // End of logout
+
+function clearForm(oForm) {
+
+	var elements = oForm.elements;
+	oForm.reset();
+	for(i=0; i<elements.length; i++) {
+
+		field_type = elements[i].type;
+		switch(field_type) {
+
+			case "text":
+			case "password":
+			case "textarea":
+
+				elements[i].value = "";
+				break;
+
+			case "radio":
+			case "checkbox":
+					if (elements[i].checked)
+						elements[i].checked = false;
+				break;
+
+			case "select-one":
+			case "select-multi":
+									elements[i].selectedIndex = 0;
+				break;
+
+			default:
+				break;
+		}
+	}
+} // End of clearForm
 
 function CheckAllBoxes(fmobj) {
   for (var i=0;i<fmobj.elements.length;i++) {
@@ -27,7 +106,7 @@ function switch_vis(element,display) {
 	var e_ref="";
 	var ant="";
 	e_ref=document.getElementById(element);
-	if (display == undefined)	{
+	if (display == undefined) {
 		display='block';
 	}
 	ant=e_ref.style.display;
@@ -39,6 +118,27 @@ function switch_vis(element,display) {
 	}
 	e_ref.style.display=display;
 } // End of switch_vis
+
+function switch_vis_mult(elements) {
+	var i=0;
+	for(i=0; i<elements.length; i++) {
+		switch_vis(elements[i],'none');
+	}
+} // End of switch_vis_mult
+
+function switch_class_mult(elements,className) {
+	var i=0;
+	for(i=0; i<elements.length; i++){
+		setClass(elements[i],className);
+	}
+} // End of switch_class_mult
+
+function elementShow(element){
+	var e_ref="";
+	var display="block";
+	e_ref=document.getElementById(element);
+	e_ref.style.display=display;
+} // End of elementShow
 
 function switch_value(element,value) {
 	var e_ref="";
@@ -57,29 +157,21 @@ function switch_value(element,value) {
 	e_ref.value=value;
 } // End of switch_value
 
-function switch_vis_mult(elements) {
-	var i=0;
-	for(i=0; i<elements.length; i++) {
-		switch_vis(elements[i],'none');
-	}
-} // End of switch_vis_mult
-
-
 function addConfigAttribute(li) {
 	ul = document.getElementById(li.id+"_ul");
 	newName=window.prompt("Nombre del nuevo atributo:",'');
 	ul.innerHTML += "<li>"+newName+": <input type='text' name='"+li.id+"["+newName+"]' value='' />"+
-		'<a class="a_image" href="#" onclick="javascript:deleteConfigAttribute(this.parentNode)">'+
+		'<a class="configIcon" href="#" onclick="javascript:deleteConfigAttribute(this.parentNode)">'+
 		'<img src="images/delete-comment-blue.gif" alt="Eliminar" /></a></li>';
 } // End of addConfigAttribute
 
 function addConfigSection(li) {
 	ul = document.getElementById(li.id+"_ul");
-	newName=window.prompt("Nombre de la nueva seccion:",'');
+	newName=window.prompt("Nombre de la nueva sección:",'');
 	ul.innerHTML += "<li id='"+li.id+"["+newName+"]'>"+newName+
-		' <a class="a_image" href="#" onclick="javascript:addConfigAttribute(this.parentNode)"><img src="images/add-comment-blue.gif" alt="Agregar Atributo" title="Agregar Atributo" /></a>'+
-		' <a class="a_image" href="#" onclick="javascript:addConfigSection(this.parentNode)"><img src="images/add-folder-green.gif" alt="Agregar Secci&oacute;n" title="Agregar Secci&oacute;n" /></a>'+
-		' <a class="a_image" href="#" onclick="javascript:deleteConfigAttribute(this.parentNode)">'+
+		' <a class="configIcon" href="#" onclick="javascript:addConfigAttribute(this.parentNode)"><img src="images/add-comment-blue.gif" alt="Agregar Atributo" title="Agregar Atributo" /></a>'+
+		' <a class="configIcon" href="#" onclick="javascript:addConfigSection(this.parentNode)"><img src="images/add-folder-green.gif" alt="Agregar Secci&oacute;n" title="Agregar Secci&oacute;n" /></a>'+
+		' <a class="configIcon" href="#" onclick="javascript:deleteConfigAttribute(this.parentNode)">'+
 		'<img src="images/delete-folder-green.gif" alt="Eliminar" /></a>'+
 		"<ul id='"+li.id+"["+newName+"]_ul'></ul></li>";
 } // End of addConfigSection
@@ -89,6 +181,10 @@ function deleteConfigAttribute(li) {
 	ul.removeChild(li);
 } // End of deleteConfigAttribute
 
+function printFunction() {
+	self.print();
+	window.close();
+} // End of printFunction
 
 
 var myGlobalHandlers = {
@@ -234,6 +330,29 @@ function ordersSendOrdersExportSaf(form) {
 <!-- End of Orders -->
 
 
+
+
+<!-- Script usado para hacer un checkbox masivo -->
+
+<!-- Begin
+var checkflag = "false";
+function check(field) {
+	if (checkflag == "false") {
+		for (i = 0; i < field.length; i++) {
+			field[i].checked = true;
+		}
+		checkflag = "true";
+		return "Deseleccionar Todos";
+	}
+	else{
+		for (i = 0; i < field.length; i++){
+			field[i].checked = false;
+		}
+		checkflag = "false";
+		return "Seleccionar Todos";
+	}
+}
+//  End -->
 
 function cambiaclase(element,clase) {
 	var NAME = document.getElementById(element);
