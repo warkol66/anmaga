4acb413b9cf2381f162b2225959609e7f7ed7f29
gflamerich <gflamerich@bcae11b6-a42b-0410-a216-9ef1f29c42dc>
2012-02-07 12:49:30 +0000

    * Comprobación si se quiere agregar producto inexistente

--- WEB-INF/classes/modules/orders/actions/OrdersAddItemToCartXAction.php
+++ WEB-INF/classes/modules/orders/actions/OrdersAddItemToCartXAction.php
@@ -25,31 +25,35 @@ class OrdersAddItemToCartXAction extends BaseAction {
 		
 		$items = $_SESSION["orderItems"];
 		$product = ProductPeer::getByCode($_POST["productCode"]);
-		$productCode = $product->getCode();
-
-		$found = false;
-		$i = 0;
-		//Busco si no esta ya este producto
-		while ($i<count($items) && !$found) {
-			$item = $items[$i];
-			if ($item->getProductCode() == $productCode) {
-				$actualQuantity = $item->getQuantity();
-				$item->setQuantity($actualQuantity + $_POST["quantity"]);
-				$_SESSION["orderItems"][$i] = $item;
-				$found = true;
+		if (!empty($product)) {
+			$productCode = $product->getCode();
+	
+			$found = false;
+			$i = 0;
+			//Busco si no esta ya este producto
+			while ($i<count($items) && !$found) {
+				$item = $items[$i];
+				if ($item->getProductCode() == $productCode) {
+					$actualQuantity = $item->getQuantity();
+					$item->setQuantity($actualQuantity + $_POST["quantity"]);
+					$_SESSION["orderItems"][$i] = $item;
+					$found = true;
+				}
+				$i++;
 			}
-			$i++;
+			//Si no estaba, lo agrego
+			if (!$found) {
+				$orderItem = new OrderItem();
+				$orderItem->setQuantity($_POST["quantity"]);
+	    		$orderItem->setProduct($product);
+	    		$orderItem->setPrice($product->getPrice());
+				$_SESSION["orderItems"][] = $orderItem;
+			}
+	
+			return $mapping->findForwardConfig('success');
 		}
-		//Si no estaba, lo agrego
-		if (!$found) {
-			$orderItem = new OrderItem();
-			$orderItem->setQuantity($_POST["quantity"]);
-    		$orderItem->setProduct($product);
-    		$orderItem->setPrice($product->getPrice());
-			$_SESSION["orderItems"][] = $orderItem;
-		}
-
-		return $mapping->findForwardConfig('success');
+		else
+			return $mapping->findForwardConfig('failure');
 	}
 
 }
